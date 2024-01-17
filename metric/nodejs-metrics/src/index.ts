06e91ef0e02263b3a8ccf941a8a9741fde556948
Rubem Dario <rubemnascimento81@gmail.com>
2024-01-17 20:00:00 +0100

    add prometheus

--- metric/nodejs-metrics/src/index.ts
+++ metric/nodejs-metrics/src/index.ts
@@ -1,10 +1,34 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
+import * as prometheus from 'prom-client';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript Express!");
+// Configure the Prometheus
+//let collectDefaultMetrics = prometheus.collectDefaultMetrics;
+const register = new prometheus.Registry();
+//prometheus.collectDefaultMetrics({ register });
+
+// Create custom metrics
+const customCounter = new prometheus.Counter({
+  name: "total_http_requests",
+  help: "Custom counter for my application",
+});
+
+// Add your custom metric to the registry
+register.registerMetric(customCounter);
+
+prometheus.collectDefaultMetrics({ register });
+
+app.get('/', (req: Request, res: Response) => {
+  customCounter.inc();
+  res.send('Hello Word ^_^');
+});
+
+// Route for prometheus metric
+app.get('/metrics', async (req: Request, res: Response) => {
+  res.set('Content-Type', register.contentType);
+  res.send(await register.metrics());
 });
 
 app.listen(port, () => {
