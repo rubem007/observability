879e8e489c9f65cba96626376168ff947a5b5c3b
Rubem Dario <rubemnascimento81@gmail.com>
2024-02-09 14:55:50 +0100

    setup otel

--- trace/laravel-app/routes/api.php
+++ trace/laravel-app/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Spatie\OpenTelemetry\Facades\Measure;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,21 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/', function () {
+    Measure::start('parent');
+     echo "1234";
+     Measure::start('child');
+     echo "4321";
+     Measure::stop('child');
+    Measure::stop('parent');
+
+    return response()->json([
+        'success' => true,
+    ]);
+});
+
+Route::get('/users', [UserController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
